dacb4613913622f18a2d1d5363460da6df44ff3f
Li Dong <cnlidong@live.cn>
2023-04-13 15:12:01 +0800

    增加前端

--- modules/modules.go
+++ modules/modules.go
@@ -1,8 +1,6 @@
 package modules
 
-import (
-	_ "github.com/cool-team-official/cool-admin-go/modules/base"
-	_ "github.com/cool-team-official/cool-admin-go/modules/dict"
-	_ "github.com/cool-team-official/cool-admin-go/modules/space"
-	_ "github.com/cool-team-official/cool-admin-go/modules/task"
-)
+// _ "github.com/cool-team-official/cool-admin-go/modules/base"
+// _ "github.com/cool-team-official/cool-admin-go/modules/dict"
+// _ "github.com/cool-team-official/cool-admin-go/modules/space"
+// _ "github.com/cool-team-official/cool-admin-go/modules/task"
